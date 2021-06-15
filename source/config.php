7030782ecc67efe9f59b55b1bb5e81e5d1d1b043
suphafang <suphachai.fang@gmail.com>
2021-06-15 18:26:39 +0700

    Remove unrelated files and add some new features

--- source/config.php
+++ source/config.php
@@ -1,10 +1,15 @@
 <?php
 	date_default_timezone_set("Asia/Bangkok");
 
+	// define('HOST_HOSTNAME', 'localhost');
+	// define('HOST_USER', 'u693904119_prepro');
+	// define('HOST_PASS', '@Pre-programming2021');
+	// define('HOST_DATABASE', 'u693904119_prepro');
+
 	define('HOST_HOSTNAME', 'localhost');
-	define('HOST_USER', 'u693904119_prepro');
-	define('HOST_PASS', '@Pre-programming2021');
-	define('HOST_DATABASE', 'u693904119_prepro');
+	define('HOST_USER', 'root');
+	define('HOST_PASS', '');
+	define('HOST_DATABASE', 'pre_programming');
 
 	class conn {
 		static public function mysqli() {
