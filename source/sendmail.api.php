1380b9d2d357855c3fc66a209b2abaa5a04c22d1
suphafang <71120866+suphafang@users.noreply.github.com>
2021-06-21 16:29:41 +0700

    Update sendmail.api.php

--- source/sendmail.api.php
+++ source/sendmail.api.php
@@ -75,7 +75,7 @@
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'pre-programming@codingtime.dev';                     //SMTP username
+            $mail->Username   = 'no-reply@codingtime.dev';                     //SMTP username
             $mail->Password   = '@Pre-programming64';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
